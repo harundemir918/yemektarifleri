f287f8a9432e21e224180c1d3e78cb5746728673
Harun Demir <harun.demir918@outlook.com>
2021-08-22 23:33:56 +0300

    hesabım sayfası eklendi

--- lib/view/account/account_view.dart
+++ lib/view/account/account_view.dart
@@ -1,13 +1,100 @@
 import 'package:flutter/material.dart';
 
-class AccountView extends StatelessWidget {
-  const AccountView({Key? key}) : super(key: key);
+import 'account_recipes_view.dart';
 
+class AccountView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final deviceWidth = MediaQuery.of(context).size.width;
+    final deviceHeight = MediaQuery.of(context).size.height;
+
     return Container(
+      height: deviceHeight,
+      width: deviceWidth,
       child: Center(
-        child: Text('Hesabım'),
+        child: ListView(
+          padding: EdgeInsets.fromLTRB(24, 36, 24, 24),
+          children: [
+            Card(
+              child: Row(
+                children: [
+                  Container(
+                    padding: EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(50),
+                    ),
+                    child: Image.asset(
+                      'assets/icons/userprofile.png',
+                      height: 100,
+                      width: 100,
+                    ),
+                  ),
+                  SizedBox(
+                    width: 16,
+                  ),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Yemektarifleri',
+                          style: Theme.of(context).textTheme.headline6,
+                        ),
+                        SizedBox(
+                          width: 12,
+                        ),
+                        Text('yemek@tarifleri.com'),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 24,
+            ),
+            Card(
+              child: Container(
+                padding: EdgeInsets.all(10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    ListTile(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => AccountRecipesView(),
+                          ),
+                        );
+                      },
+                      contentPadding: EdgeInsets.zero,
+                      visualDensity: VisualDensity.compact,
+                      title: Text('Tariflerim'),
+                      trailing: Icon(
+                        Icons.chevron_right,
+                        size: 20,
+                      ),
+                    ),
+                    SizedBox(height: 16),
+                    Center(
+                      child: TextButton(
+                        onPressed: () {},
+                        child: Text(
+                          "Çıkış Yap",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        style: TextButton.styleFrom(
+                          backgroundColor: Colors.deepOrange,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }

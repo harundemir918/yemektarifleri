7eff4cb758f290404faaae4d4513b6f875ae4d67
Harun Demir <harun.demir918@outlook.com>
2021-09-11 13:31:06 +0300

    register sayfası düzenlendi

--- lib/view/auth/register_view.dart
+++ lib/view/auth/register_view.dart
@@ -10,7 +10,12 @@ class RegisterView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    Constants _constants = Constants();
+    TextEditingController _emailController = TextEditingController();
+    TextEditingController _passwordController = TextEditingController();
+    TextEditingController _passwordAgainController = TextEditingController();
+
+    final _formKey = GlobalKey<FormState>();
+    bool _isLoading = false;
 
     return Scaffold(
       body: SingleChildScrollView(
@@ -21,76 +26,127 @@ class RegisterView extends StatelessWidget {
           ),
           width: MediaQuery.of(context).size.width,
           height: MediaQuery.of(context).size.height,
-          child: Column(
-            children: <Widget>[
-              Container(height: 25),
-              AppLogo(),
-              Container(height: 5),
-              Text(
-                _constants.createNewAccountString,
-                style: TextStyle(
-                  color: Colors.grey,
-                ),
-              ),
-              Spacer(),
-              AuthTextFormField(
-                label: _constants.emailString,
-                obscured: false,
-              ),
-              Container(height: 25),
-              AuthTextFormField(
-                label: _constants.passwordString,
-                obscured: true,
-              ),
-              Container(height: 25),
-              AuthTextFormField(
-                label: _constants.passwordAgainString,
-                obscured: true,
-              ),
-              Container(height: 25),
-              Container(
-                width: double.infinity,
-                height: 40,
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    primary: Colors.red,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                  ),
-                  child: Text(
-                    _constants.signUpString,
+          child: Form(
+            key: _formKey,
+            child: Container(
+              child: Column(
+                children: [
+                  Container(height: 25),
+                  AppLogo(),
+                  Container(height: 5),
+                  Text(
+                    loginToYourAccountString,
                     style: TextStyle(
-                      color: Colors.white,
+                      color: Colors.grey,
                     ),
                   ),
-                  onPressed: () {},
-                ),
-              ),
-              Container(
-                width: double.infinity,
-                child: TextButton(
-                  style: TextButton.styleFrom(
-                    primary: Colors.transparent,
+                  Spacer(),
+                  AuthTextFormField(
+                    label: emailString,
+                    controller: _emailController,
+                    keyboard: TextInputType.emailAddress,
+                    obscured: false,
                   ),
-                  child: Text(
-                    _constants.doYouHaveAnAccountString,
-                    style: TextStyle(
-                      color: Colors.red,
-                    ),
+                  SizedBox(
+                    height: 25,
                   ),
-                  onPressed: () {
-                    Navigator.pushReplacement(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => LoginView(),
+                  AuthTextFormField(
+                    label: passwordString,
+                    controller: _passwordController,
+                    keyboard: TextInputType.text,
+                    obscured: true,
+                  ),
+                  SizedBox(
+                    height: 25,
+                  ),
+                  AuthTextFormField(
+                    label: passwordAgainString,
+                    controller: _passwordAgainController,
+                    keyboard: TextInputType.text,
+                    obscured: true,
+                  ),
+                  SizedBox(
+                    height: 25,
+                  ),
+                  Container(
+                    width: double.infinity,
+                    height: 40,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.red,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
                       ),
-                    );
-                  },
-                ),
+                      child: Text(
+                        loginString,
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                      onPressed: () async {
+                        // if (_formKey.currentState!.validate()) {
+                        //   setState(() {
+                        //     _isLoading = true;
+                        //   });
+                        //   Api api = Api();
+                        //   bool _isLoggedIn = await api.logIn(
+                        //       email: _email.text.toString(),
+                        //       password: _password.text.toString());
+                        //   if (_isLoggedIn) {
+                        //     setState(() {
+                        //       _isLoading = false;
+                        //     });
+                        //     Navigator.pushReplacement(
+                        //       context,
+                        //       MaterialPageRoute(
+                        //         builder: (context) => HomeView(),
+                        //       ),
+                        //     );
+                        //   } else {
+                        //     setState(() {
+                        //       _isLoading = false;
+                        //     });
+                        //     ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                        //       content: Text("Bilgilerinizi kontrol ediniz."),
+                        //     ));
+                        //   }
+                        // }
+                      },
+                    ),
+                  ),
+                  Container(
+                    width: double.infinity,
+                    child: TextButton(
+                      style: TextButton.styleFrom(primary: Colors.transparent),
+                      child: Text(
+                        doYouHaveAnAccountString,
+                        style: TextStyle(
+                          color: primaryColor,
+                        ),
+                      ),
+                      onPressed: () {
+                        Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => LoginView(),
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  _isLoading == true
+                      ? CircularProgressIndicator(
+                          valueColor:
+                              AlwaysStoppedAnimation<Color?>(primaryColor),
+                        )
+                      : Container(),
+                ],
               ),
-              Container(height: 20),
-            ],
+            ),
           ),
         ),
       ),

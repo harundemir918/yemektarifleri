c89c01cdd2f71264aa78511d0ee1be421d407c54
Harun Demir <harun.demir918@outlook.com>
2021-09-12 12:53:22 +0300

    main'e provider eklendi

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
+import 'constants.dart';
 import 'view/auth/login_view.dart';
+import 'viewmodel/recipe_list_view_model.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Material App',
-      home: LoginView(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (BuildContext context) => RecipeListViewModel(),
+        ),
+      ],
+      child: MaterialApp(
+        title: appName,
+        home: LoginView(),
+      ),
     );
   }
 }

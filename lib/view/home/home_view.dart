bcece21975b70faccef834fedb2c2259fcfc9edf
Harun Demir <harun.demir918@outlook.com>
2021-09-20 15:27:32 +0300

    tokene göre işlemler yapıldı

--- lib/view/home/home_view.dart
+++ lib/view/home/home_view.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
+import '../../service/api.dart';
 import '../account/account_view.dart';
 import '../add_recipe/add_recipe_view.dart';
+import '../auth/login_view.dart';
 import '../recipes/recipes_list_view.dart';
 
 class HomeView extends StatefulWidget {
@@ -13,6 +16,8 @@ class _HomeViewState extends State<HomeView>
     with SingleTickerProviderStateMixin {
   late TabController _tabController;
   int _currentIndex = 0;
+  int? userId;
+  int? userActive;
 
   final List<Widget> _pageList = [
     RecipesListView(),
@@ -29,6 +34,16 @@ class _HomeViewState extends State<HomeView>
       initialIndex: _currentIndex,
     );
     _tabController.addListener(() {});
+    getUserInfo();
+  }
+
+  getUserInfo() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    setState(() {
+      userId = prefs.getInt('id') ?? null;
+      userActive = prefs.getInt('active') ?? null;
+    });
   }
 
   @override
@@ -41,14 +56,43 @@ class _HomeViewState extends State<HomeView>
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: SingleChildScrollView(
-          child: GestureDetector(
-            onTap: () => FocusScope.of(context).unfocus(),
-            child: Container(
-              child: _pageList.elementAt(_currentIndex),
-            ),
-          ),
-        ),
+        child: userActive == 1
+            ? SingleChildScrollView(
+                child: GestureDetector(
+                  onTap: () => FocusScope.of(context).unfocus(),
+                  child: Container(
+                    child: _pageList.elementAt(_currentIndex),
+                  ),
+                ),
+              )
+            : Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Hesabınız aktif değildir. Lütfen yönetici ile görüşün.',
+                    ),
+                    TextButton(
+                      onPressed: () async {
+                        await Api().setToken('');
+                        Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => LoginView(),
+                          ),
+                        );
+                      },
+                      child: Text(
+                        "Çıkış Yap",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      style: TextButton.styleFrom(
+                        backgroundColor: Colors.deepOrange,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
       ),
       bottomNavigationBar: Card(
         elevation: 4,

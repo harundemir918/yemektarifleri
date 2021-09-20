17bd1ddf463b1226d33d210b9b50cb7fe5696341
Harun Demir <harun.demir918@outlook.com>
2021-09-20 15:27:18 +0300

    deviceWidth deviceHeight değerleri kaldırıldı

--- lib/view/widgets/categories_grid_list.dart
+++ lib/view/widgets/categories_grid_list.dart
@@ -9,14 +9,10 @@ class CategoriesGridList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final deviceWidth = MediaQuery.of(context).size.width;
-    final deviceHeight = MediaQuery.of(context).size.height;
-
     return Container(
       child: Column(
         children: [
-          Container(
-            height: deviceHeight * 0.89,
+          Expanded(
             child: GridView.count(
               crossAxisCount: 2,
               padding: EdgeInsets.all(25.0),
